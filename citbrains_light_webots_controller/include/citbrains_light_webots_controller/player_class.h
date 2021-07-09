435a7dada48a17ac4c4a483ff4e03fb8db91fe0a
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2021-07-09 19:59:23 +0900

    add new class

---
+++ citbrains_light_webots_controller/include/citbrains_light_webots_controller/player_class.h
@@ -0,0 +1,26 @@
+#include <webots/Motor.hpp>
+#include <webots/Robot.hpp>
+#include <webots/Accelerometer.hpp>
+#include <webots/Gyro.hpp>
+#include <webots/Keyboard.hpp>
+#include <webots/Camera.hpp>
+#include <cmath>
+#include <set>
+#include <exception>
+#include <cstdint>
+#include <tuple>
+#include <cstdint>
+#include <string>
+#include "controllmessage.pb.h"
+
+class Player{
+    public:
+    //controllerクラスでメッセージとして受け取ったbyte列をパースしてデータをセットする.stringはperfect forwardしたい
+    parseAndSetMessageFromByteSequence(std::string&& bytes);
+    
+    private:
+    int32_t robot_id_;
+    //関節角のセット.
+    setMotorAngles();
+    
+};
---
+++ citbrains_light_webots_controller/src/player_class.cpp
@@ -0,0 +1,1 @@
+#include "playerclass.h"
